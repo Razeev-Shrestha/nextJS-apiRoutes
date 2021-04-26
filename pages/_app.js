2aa97d7cbcd3c02356181870e9810b051a20dcb2
Razeev-stha <yuvrajstha1246@outlook.com>
2021-04-26 18:20:26 +0545

    nextjs api practice

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { Fragment } from 'react'
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+	return (
+		<Fragment>
+			<Head>
+				<title>Next API </title>
+				<meta name='description' content='NextJs API' />
+				<meta
+					name='viewport'
+					content='initial-scale=1.0,width=device-width'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	)
 }
 
 export default MyApp
